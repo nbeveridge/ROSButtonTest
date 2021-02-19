c44e0172121ba327644aef096a76d259452fdf7d
nbeveridge <60130433+nbeveridge@users.noreply.github.com>
2021-02-19 10:28:53 -0700

    Delete button_state_publisher.py

--- button_state_publisher.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import rospy
-import RPi.GPIO as GPIO
-BUTTON_GPIO = 16
-if __name__ == '__main__':
-    rospy.init_node('button_state_publisher')
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down = GPIO.PUD_UP)
-    GPIO.cleanup()
